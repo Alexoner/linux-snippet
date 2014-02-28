8d09bb72c0bce1372859e8d03b61b1203d999c28
Alexoner <onerhao@gmail.com>
2014-02-28 11:16:31 +0800

    event.input:use ioctl(fd,EVIOCGNAME,evname) to get /dev/input/event* device name

--- event/input/keylog.c
+++ event/input/keylog.c
@@ -13,6 +13,7 @@
  * Sleep Button entry:
  * [    34.744] (II) config/udev: Adding input device Sleep Button (/dev/input/event1)
  *
+ * Existing project dealing with linux event are: acpid,systemd,X11.
  *
  * POLL:
  *
@@ -27,6 +28,85 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+
+struct connection
+{
+    /* file descriptor */
+    int fd;
+
+    /* process incoming data on the connection */
+    /* ??? suggest passing a pointer to this connection struct */
+    void (* process)(int fd);
+
+    /* Optional.  Used by find_connection_name() to find the connection for a
+       specific file.  Set to NULL if not specified.  Memory will be freed
+       with free() when connection is deleted. */
+    char *pathname;
+
+    /* 0 indicates this is probably not a keyboard device */
+    int kybd;
+};
+
+
+/*-----------------------------------------------------------------*
+ * open a single input layer file for input  */
+int open_inputfile(const char *filename)
+{
+    int fd;
+    struct connection c;
+
+    /* O_CLOEXEC: Make sure scripts we exec() (in event.c) don't get our file
+       descriptors. */
+    fd = open(filename, O_RDONLY | O_NONBLOCK | O_CLOEXEC);
+    /*fd = open(filename, O_RDONLY);*/
+
+    if (fd >= 0)
+    {
+        char evname[256];
+
+        /* if this file doesn't have events we need, indicate failure */
+        /*if (!has_event(fd))*/
+        /*{*/
+        /*close(fd);*/
+        /*return -1;*/
+        /*}*/
+
+        /* get this event file's name for debugging */
+        strcpy(evname, "Unknown");
+        printf("opened file %s (%s)\n", filename, evname);
+        ioctl(fd, EVIOCGNAME(sizeof(evname)), evname);
+
+        /*acpid_log(LOG_DEBUG, "input layer %s (%s) "*/
+        printf( "input layer %s (%s) "
+                "opened successfully, fd %d\n", filename, evname, fd);
+
+        /* add a connection to the list */
+        c.fd = fd;
+        /*c.process = process_input;*/
+        /* delete_connection() will free */
+        c.pathname = malloc(strlen(filename) + 1);
+        if (c.pathname)
+            strcpy(c.pathname, filename);
+        /* assume not a keyboard until we see a scancode */
+        c.kybd = 0;
+
+        /*if (add_connection(&c) < 0)*/
+        /*{*/
+        /*close(fd);*/
+        /*acpid_log(LOG_ERR,*/
+        /*"can't add connection for input layer %s (%s)",*/
+        /*filename, evname);*/
+        /*return -1;*/
+        /*}*/
+
+        return fd;  /* success */
+    }
+
+    /* open unsuccessful */
+    return 0;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -36,7 +116,8 @@ int main(int argc, char **argv)
         printf("usage: %s <device>\n", argv[0]);
         return 1;
     }
-    fd = open(argv[1], O_RDONLY);
+    /*fd = open(argv[1], O_RDONLY);*/
+    fd = open_inputfile(argv[1]);
     if (fd < 0)
     {
         perror(argv[1]);
@@ -48,11 +129,15 @@ int main(int argc, char **argv)
     {
         read(fd, &ev, sizeof(struct input_event));
 
-        if (ev.type == 1)
+        if (ev.type == EV_KEY)
+        {
             printf("[%ld.%06ld] key %i state %i\n",
                    ev.time.tv_sec,
                    ev.time.tv_usec,
                    ev.code, ev.value);
+            printf("The pressed key is: %s\n",
+                   ev.code == KEY_POWER ? "power" : "suspend");
+        }
 
     }
 }
