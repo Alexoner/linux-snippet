1bb7af6150aac307309d8f6eef4418b05876ed22
onerhao <onerhao@gmail.com>
2015-01-27 20:42:48 +0800

    grep files containing a certain pattern

--- shell/find-file-containing.sh
+++ shell/find-file-containing.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 find . -regextype egrep -regex ".*.(java|vm|xml|js)" |xargs -0 grep --color=auto -slnie "tr_tr"
+
+grep --include=\*.{c,h} -rnw . -e "pattern"
+grep --exclude=*.o -rnw 'directory' -e "pattern"
+grep --exclude-dir={dir1,dir2,*.dst} -rnw 'directory' -e "pattern"
