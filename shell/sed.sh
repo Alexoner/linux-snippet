4e829fbd82b7a09b4163101d0b81e7449ef5d9fc
onerhao <onerhao@gmail.com>
2015-01-27 21:13:29 +0800

    replace in place

--- shell/sed.sh
+++ shell/sed.sh
@@ -4,4 +4,4 @@
 echo "200 792414 \"http://www.alibaba-inc.com/param1=1&param2=4\" \"http://www.fuckjingdong.com/yes/\" This is a log " |
     sed "s/.*\(200 [0-9]* \"[^\"]*\"\).*/\1/"
     
-
+sed -i 's/oldstring/newstring/g' file.txt
