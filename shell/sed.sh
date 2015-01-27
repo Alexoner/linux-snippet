d6aa1bc90d79776b133657361337332c68499a0a
onerhao <onerhao@gmail.com>
2015-01-27 21:05:49 +0800

    extract with sed

---
+++ shell/sed.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# extract regular expression pattern with sed.Use "[]" to work around the problem that ungreedy search isn't supported
+echo "200 792414 \"http://www.alibaba-inc.com/param1=1&param2=4\" \"http://www.fuckjingdong.com/yes/\" This is a log " |
+    sed "s/.*\(200 [0-9]* \"[^\"]*\"\).*/\1/"
+    
+
